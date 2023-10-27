75fdf54618904ecb83f018f4f3aba190d2c4da2f
anyalenzy <anya.dudnik.91@gmail.com>
2023-10-27 22:39:10 +0300

    done task-1

---
+++ js/task-1.js
@@ -0,0 +1,13 @@
+"use strict";
+function isEnoughCapacity(products, containerSize) {
+  const productsValue = Object.values(products);
+  let sum = 0;
+  for (const val of productsValue) {
+    sum += val;
+  }
+  return sum <= containerSize ? true : false;
+}
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8));
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12));
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14));
+console.log(isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7));
